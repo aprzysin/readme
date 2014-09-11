d3be41b5341bf7d29dab2e2ed7c086a5650a2b21
aprzysin <aprzysin@skidmore.edu>
2014-09-11 06:13:24 -0700

    dogtestthursday
    ...

--- chpt1_1/src/chpt1_1/Project_1.java
+++ chpt1_1/src/chpt1_1/Project_1.java
@@ -13,28 +13,7 @@ public class Project_1 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// declare variables
-	  int CurrentExp;
-	  int CurrentPrestige;
-	  int PrestigeGoal;
-	  int ExpTillPrestige;
-	  int ExpTillPrestigeGoal;
-	  
-	  	//user input
-	  CurrentExp = 1000000;
-	  CurrentPrestige = 3;
-	  PrestigeGoal = 5;
-	  	//compute
-	  ExpTillPrestige = 1202800 - CurrentExp;
-	  ExpTillPrestigeGoal = (1202800*PrestigeGoal) - (1202800*CurrentPrestige) - CurrentExp;
-	  
-	  //print results
-	  System.out.print("Exp Needed To Pretige: ");
-	  System.out.println(ExpTillPrestige);
-	  System.out.print("Exp Needed For Pretige Level ");
-	  System.out.print(PrestigeGoal);
-	  System.out.print(": ");
-	  System.out.print(ExpTillPrestigeGoal);
+		PrestigeCalc();
 	  
 	  
 	  
@@ -43,7 +22,23 @@ public class Project_1 {
 		// TODO Auto-generated method stub
 
 	}
-	public static void Output() {
+	public static void PrestigeCalc() {
+		// declare variables
+		  int CurrentExp;
+		  int CurrentPrestige;
+		  int PrestigeGoal;
+		  int ExpTillPrestige;
+		  int ExpTillPrestigeGoal;
+		  
+		  	//user input
+		  CurrentExp = 1000000;
+		  CurrentPrestige = 3;
+		  PrestigeGoal = 5;
+		  	//compute
+		  ExpTillPrestige = 1202800 - CurrentExp;
+		  ExpTillPrestigeGoal = (1202800*PrestigeGoal) - (1202800*CurrentPrestige) - CurrentExp;
+		  
+		  //print results
 		  System.out.print("Exp Needed To Pretige: ");
 		  System.out.println(ExpTillPrestige);
 		  System.out.print("Exp Needed For Pretige Level ");
